c7a169785058ce9a63c702c0598203bfb66d8a9c
Murali Krishna <murali1125s@gmail.com>
2022-06-11 08:22:57 +0530

    initial commit

---
+++ BasicIntro/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicIntro
+{
+    internal class Demo
+    {
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="a"></param>
+      /// <param name="b"></param>
+        public void Start(int a, string b)
+        {
+
+        }
+    }
+}
